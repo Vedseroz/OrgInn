dc6360eed6e35c5c108d78f7947578a812866ece
Vicho Salgado J <vicente_tsj@hotmail.com>
2019-08-08 18:42:22 -0400

    AAAAAAAAAAAAAAAAAAAAAA

--- src/proyecto/Operador.java
+++ src/proyecto/Operador.java
@@ -3,9 +3,8 @@ package proyecto;
 import java.util.ArrayList;
 
 public interface Operador{
-
 	public abstract void agregar(Object o);
-	public abstract ArrayList<Persona> obtener();
+	public abstract Object obtener();
 	public abstract void eliminar();
 	
 }
